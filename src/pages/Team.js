65f68efb5a98376f25a9371cd7d2310c27de2a81
Alex Scott <alex@alexscott.net>
2018-08-08 10:09:11 +0200

    CA-249: Team page.

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,9 +1,13 @@
 import React from "react"
-import { graphql } from "gatsby"
+//import { graphql } from "gatsby"
 
 import GeneralLayout from "./../components/layouts/GeneralLayout"
 
-const Team = () => {
+import OurPerspectives from '../components/organisms/OurPerspectives';
+
+const Team = ({data}) => {
+
+    console.log("DATA, ", data);
 
   return(
     <GeneralLayout
@@ -11,9 +15,47 @@ const Team = () => {
       heroSubtitle = "We are a fun loving, open hearted group of civic technology professionals committed to making life better for our clients and each other."
     >
 
+      About:
+      <p />
 
+      <OurPerspectives /> 
     </GeneralLayout>
   )
 };
 
 export default Team;
+
+
+// export const query = graphql`
+//   query AboutQuery {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//   }
+`
+
+
+export const OBJ = graphql `
+query AllTeamNodes() {
+    allMarkdownRemark(filter: {frontmatter: {type: {eq: "team"}}}) {
+        edges {
+            node {
+                headings {
+                    depth
+                    value
+                }
+                frontmatter {
+          name 
+          image {
+            id
+          } 
+        }
+      }
+    }
+  }
+}
+`;
+
+
